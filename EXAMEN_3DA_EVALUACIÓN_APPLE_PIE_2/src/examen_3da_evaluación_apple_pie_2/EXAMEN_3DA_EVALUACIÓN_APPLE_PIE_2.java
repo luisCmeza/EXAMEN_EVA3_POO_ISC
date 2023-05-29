7e927a9342dfd230bfd6e39e25d7195ea0155181
LuisC <124108074+luisCmeza@users.noreply.github.com>
2023-05-28 21:13:28 -0600

    Add files via upload

---
+++ EXAMEN_3DA_EVALUACIÓN_APPLE_PIE_2/src/examen_3da_evaluación_apple_pie_2/EXAMEN_3DA_EVALUACIÓN_APPLE_PIE_2.java
@@ -0,0 +1,71 @@
+
+package examen_3da_evaluación_apple_pie_2;
+
+/*
+INTEGRANTES:
+ANGEL EDUARDO ARIZPE CARRILLO
+IRAM ARREDONDO RAMÍREZ
+DAVID ALBERTO MARTÍNEZ ESPINOZA
+LUIS CARLOS MEZA LÓPEZ
+*/
+
+public class EXAMEN_3DA_EVALUACIÓN_APPLE_PIE_2 {
+
+    
+    public static void main(String[] args) {
+        //Se crea un objeto "inventario" para poder hacer uso de los métodos de la clase Inventario
+        Inventario inventario = new Inventario();
+        
+        Producto p1 = new Producto(1, "Avena", 20, 4);
+        inventario.addProduct(p1);
+        
+        Producto p2 = new Producto(2, "Leche", 30, 5);
+        inventario.addProduct(p2);
+        
+        Producto p3 = new Producto(3, "Huevos", 60, 7);
+        inventario.addProduct(p3);
+        
+        Producto p4 = new Producto(4, "Pan", 40, 8);
+        inventario.addProduct(p4);
+        
+        Producto p5 = new Producto(5, "Atún", 10, 12);
+        inventario.addProduct(p5);
+        
+        Producto p6 = new Producto(6, "Jamón", 80, 15);
+        inventario.addProduct(p6);
+        
+        Producto p7 = new Producto(7, "Limón", 5, 20);
+        inventario.addProduct(p7);
+        
+        Producto p8 = new Producto(8, "Sabritas", 35, 9);
+        inventario.addProduct(p8);
+        
+        Producto p9 = new Producto(9, "Sodas", 45, 13);
+        inventario.addProduct(p9);
+        
+        Producto p10 = new Producto(10, "Tortillas", 15, 17);
+        inventario.addProduct(p10);
+        
+        //Producto p11 = new Producto(11, "Tortillas Maiz", 23, 4);
+        //inventario.addProduct(p11);
+        
+        //REMOVER UN PRODUCTO
+        //inventario.removeProduct(7);
+        //inventario.removeProduct(20);
+        
+        //BUSCAR UN PRODUCTO
+        //inventario.searchProduct(3);
+        
+        //MOSTAR INVENTARIO
+        //inventario.displayInventory();
+        
+        //GUARDAR EN UN ARCHIVO DE TEXTO (.txt)
+        //Ubicación --> C:\\Archivos\\
+        inventario.safeToFile("ProductosA");
+        
+        //LEER UN ARCHIVO DE TEXTO (.txt)
+        //Ubicación --> C:\\Archivos\\
+        //inventario.loadFromFile("ProductosA");
+    }
+    
+}
---
+++ EXAMEN_3DA_EVALUACIÓN_APPLE_PIE_2/src/examen_3da_evaluación_apple_pie_2/Inventario.java
@@ -0,0 +1,154 @@
+package examen_3da_evaluación_apple_pie_2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Inventario {
+
+    private Producto[] productos = new Producto[10];//Limite de objetos que se pueden crear en el arreglo productos (x<=10)
+    private int contador = 0;//Cantidad de objetos "reales" creados
+
+    //AGREGA PRODUCTOS AL SISTEMA
+    void addProduct(Producto producto) {
+        if (contador < 10) {
+            productos[contador] = producto;//Guarda en la posición cantidad el objeto producto creado en el main
+            contador++;
+        } else {
+            System.out.println("Cantidad de productos máxima alcanzada");
+            System.out.println("");
+        }
+    }
+
+    //ELIMINA UN PRODUCTO EN FUNCIÓN A SU ID
+    void removeProduct(int productId) {
+        boolean found = false;
+        for (int i = 0; i < productos.length; i++) {
+            //Verificar que exista algún producto con el id señalado
+            for (int j = 0; j < contador; j++) {
+                if (productos[j] != null && productos[j].getId() == productId) {
+                    found = true;
+                }
+            }
+            //Eliminación del objeto de la posición según el id
+            if (productos[i] != null && productos[i].getId() == productId) {
+                productos[i] = null;
+                for (int j = 1; j < contador; j++) {
+                    if (productos[i] == null && i < contador - 1) {
+                        productos[i] = productos[i + 1];
+                        i++;
+                        productos[i] = null;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            //Mensaje de error en caso de que no exista un objeto con el id señalado
+            if (found != true) {
+                System.out.println("El producto con el id '" + productId + "' a eliminar no fue encontrado");
+                System.out.println("");
+                break;
+            }
+        }
+    }
+
+    //BUSCA UN PRODUCTO POR SU ID Y DEVUELVE EL OBJETO PRODUCTO SI LO ENCUENTRA
+    String searchProduct(int productId) {
+        boolean found = false;
+        String answer = "";
+        //Verificación de que exista un objeto con el id señalado
+        for (int i = 0; i < productos.length; i++) {
+            for (int j = 0; j < contador; j++) {
+                if (productos[j] != null && productos[j].getId() == productId) {
+                    found = true;
+                }
+            }
+            //Imprime el objeto buscado según Id
+            if (productos[i] != null && productos[i].getId() == productId) {
+                System.out.println("ID: " + productos[i].getId());
+                System.out.println("Nombre: " + productos[i].getNombre());
+                System.out.println("Precio: " + productos[i].getPrecio());
+                System.out.println("Cantidad: " + productos[i].getCantidad());
+                System.out.println("");
+            }
+            //Mensaje de error en caso de que no exista el id señalado
+            if (found != true) {
+                System.out.println("El producto buscado con el id '" + productId + "' no fue encontrado");
+                break;
+            }
+        }
+        return answer;
+    }
+
+    //MUESTRA EL INVENTARIO ACTUAL CON TODOS LOS PRODUCTOS Y SUS DETALLES
+    void displayInventory() {
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i] != null) {
+                System.out.println("ID: " + productos[i].getId());
+                System.out.println("Nombre: " + productos[i].getNombre());
+                System.out.println("Precio: " + productos[i].getPrecio());
+                System.out.println("Cantidad: " + productos[i].getCantidad());
+                System.out.println("");
+            } else {
+                break;
+            }
+        }
+    }
+
+    //GUARDA TODO EL INVENTARIO EN UN SOLO ARCHIVO DE TEXTO CON EL NOMBRE DE ARCHIVO DADO
+    void safeToFile(String filename) {
+        String ruta = "C:\\Archivos\\";
+        try {
+            writeUsingBufferedWriter(ruta + filename + ".txt", "");
+        } catch (IOException ex) {
+            ex.getMessage();
+        }
+    }
+
+    //CARGA EL CONTENIDO DEL INVENTARIO DESDE UN ARCHIVO DE TEXTO CON EL NOMBRE DE ARCHIVO DADO
+    void loadFromFile(String filename) {
+        final String RUTA = "C:\\Archivos\\" + filename + ".txt";
+        try {
+            readingUsingBufferedReader(RUTA);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //MÉTODO DEL BUFFEREDWRITER
+    public void writeUsingBufferedWriter(String ruta, String datos) throws IOException {
+        File file = new File(ruta);
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+        for (int i = 0; i < 10; i++) {
+            if (productos[i] != null) {
+                bufWriter.write("ID: " + productos[i].getId() + " \n");
+                bufWriter.write("Nombre: " + productos[i].getNombre() + " \n");
+                bufWriter.write("Precio: " + productos[i].getPrecio() + " \n");
+                bufWriter.write("Cantidad: " + productos[i].getCantidad() + " \n");
+                bufWriter.write("\n");
+            }
+        }
+        bufWriter.close();
+        fileWriter.close();
+    }
+    //MÉTODO DEL BUFFEREDREADER
+    public static void readingUsingBufferedReader(String ruta) throws IOException {
+        System.out.println("");
+        File file = new File(ruta);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufReader = new BufferedReader(inputStreamReader);
+        String linea;
+        while ((linea = bufReader.readLine()) != null) {
+            System.out.println(linea);
+        }
+        bufReader.close();
+    }
+}
